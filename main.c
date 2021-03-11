a07b332cb2c975b05196b8254be5c4b410e2311f
TAKU1101 <taku.kuma1101@icloud.com>
2021-03-11 12:14:29 +0100

    Add: isblank

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include "libft.h"
+#include <ctype.h>
+#include <stdio.h>
 
 int main(void)
 {
-	
+	int i;
+	int sum;
+
+	sum = 0;
+	for (i = 0; i < 256; i++)
+	{
+		printf("%d %d\n", i, isblank(i));
+		sum += isblank(i);
+	}
+	printf("%d\n", sum);
 }
